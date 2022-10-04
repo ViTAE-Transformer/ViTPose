0dbda2748d0299dabfa1dda497ea974a8d7557bc
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 18:49:20 +0000

    Adding numeric_owner as keyword arguement

--- tools/dataset/preprocess_h36m.py
+++ tools/dataset/preprocess_h36m.py
@@ -114,7 +114,7 @@ class PreprocessH36m:
                             if not is_within_directory(path, member_path):
                                 raise Exception("Attempted Path Traversal in Tar File")
                     
-                        tar.extractall(path, members, numeric_owner) 
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
                         
                     
                     safe_extract(tar, self.extracted_dir)
