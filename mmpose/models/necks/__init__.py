7417e681eae33677c2a0706595eec8a23061426b
Taosha Fan <fantaosha@gmail.com>
2025-04-26 13:50:07 +0000

    update mmpose

--- mmpose/models/necks/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .gap_neck import GlobalAveragePooling
-from .posewarper_neck import PoseWarperNeck
-
-__all__ = ['GlobalAveragePooling', 'PoseWarperNeck']
